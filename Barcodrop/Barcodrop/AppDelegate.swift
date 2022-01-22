83669bcdcfce9d7c956ab8dc407fc4049d0a060e
Zer95 <ggrty10@gmail.com>
2022-01-22 22:49:36 +0900

    feat: Custom TabBar 추가

--- Barcodrop/Barcodrop/AppDelegate.swift
+++ Barcodrop/Barcodrop/AppDelegate.swift
@@ -6,14 +6,15 @@
 //
 
 import UIKit
+import CBFlashyTabBarController
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // TabBar Setting
+        CBFlashyTabBar.appearance().tintColor = #colorLiteral(red: 0.1176470588, green: 0.1176470588, blue: 0.431372549, alpha: 1)
+        CBFlashyTabBar.appearance().barTintColor = .white
         return true
     }
 
@@ -30,7 +31,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
 
 }
 
--- Barcodrop/Barcodrop/SceneDelegate.swift
+++ Barcodrop/Barcodrop/SceneDelegate.swift
@@ -6,18 +6,31 @@
 //
 
 import UIKit
+import CBFlashyTabBarController
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        let homeVC = HomeViewController()
+        homeVC.tabBarItem = UITabBarItem(title: "홈", image: #imageLiteral(resourceName: "Events"), tag: 0)
+        let categoryVC = CategoryViewController()
+        categoryVC.tabBarItem = UITabBarItem(title: "카테고리", image: #imageLiteral(resourceName: "Search"), tag: 0)
+        let settingVC = SettingViewController()
+        settingVC.tabBarItem = UITabBarItem(title: "설정", image: #imageLiteral(resourceName: "Highlights"), tag: 0)
+        let tabBarController = CBFlashyTabBarController()
+        tabBarController.viewControllers = [homeVC, categoryVC, settingVC]
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        
+        let mainViewController = tabBarController
+        self.window?.rootViewController  = mainViewController
+        self.window?.makeKeyAndVisible()
     }
+    
 
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
