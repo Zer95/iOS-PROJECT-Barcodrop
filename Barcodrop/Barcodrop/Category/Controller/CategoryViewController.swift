ebfcccde6d47e11629052138f2e36f31dcd3a963
Zer95 <ggrty10@gmail.com>
2022-01-27 21:05:56 +0900

    feat: [카테고리] 이미지 스와이프 제스처 추가

--- Barcodrop/Barcodrop/Category/Controller/CategoryViewController.swift
+++ Barcodrop/Barcodrop/Category/Controller/CategoryViewController.swift
@@ -17,6 +17,7 @@ class CategoryViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = UIColor.white
         configureUI()
+        imageSwipeSetting()
     }
     
     func configureUI() {
@@ -25,6 +26,41 @@ class CategoryViewController: UIViewController {
             $0.top.left.right.equalToSuperview()
             $0.height.equalTo(400)
         }
+    }
+    
+}
+
+// MARK: - HeaderTip Image Swipe
+
+extension CategoryViewController {
+    
+    func imageSwipeSetting() {
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(CategoryViewController.respondToSwipeGesture(_:)))
+        swipeLeft.direction = UISwipeGestureRecognizer.Direction.left
+        self.view.addGestureRecognizer(swipeLeft)
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(CategoryViewController.respondToSwipeGesture(_:)))
+        swipeRight.direction = UISwipeGestureRecognizer.Direction.right
+        self.view.addGestureRecognizer(swipeRight)
+    }
+    
+    @objc func respondToSwipeGesture(_ gesture: UIGestureRecognizer) {
+        
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer{
+            
+            switch swipeGesture.direction {
+            case UISwipeGestureRecognizer.Direction.left :
+                headerTipView.pageControl.currentPage -= 1
+                headerTipView.imageView.image = UIImage(named: Constant.categoryImageList[headerTipView.pageControl.currentPage])
+            case UISwipeGestureRecognizer.Direction.right :
+                headerTipView.pageControl.currentPage += 1
+                headerTipView.imageView.image = UIImage(named: Constant.categoryImageList[headerTipView.pageControl.currentPage])
+            default:
+                break
+            }
+            
+        }
+        
     }
     
 }
