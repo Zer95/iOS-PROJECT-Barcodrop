07cdeb7836a4e1e4a8d8df30ed4f6ae2761921fc
Zer95 <ggrty10@gmail.com>
2022-02-06 15:39:00 +0900

    feat: [Constant] 카테고리 모델 임시 추가

--- Barcodrop/Barcodrop/Common/Constant/Constant.swift
+++ Barcodrop/Barcodrop/Common/Constant/Constant.swift
@@ -10,11 +10,27 @@ import Foundation
 struct Constant {
     
     // 카테고리 헤더 배너 이미지
-    static let categoryImageList = [
+    static let categoryBannerImage = [
         "tip1.jpg",
         "tip2.jpg",
         "tip3.jpg",
         "tip4.jpg"
+    ]
+    
+    // 카테고리 리스트 항목
+    static let categoryList = [
+        "냉장",
+        "냉동",
+        "실온",
+        "기타"
+    ]
+    
+    // 카테고리 리스트 이미지 항목
+    static let categoryImageList = [
+        "fresh_on.png",
+        "ice_on.png",
+        "room_on.png",
+        "etc_on.png"
     ]
     
 }
