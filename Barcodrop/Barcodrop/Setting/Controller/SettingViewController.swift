04a941671616fec40f3a21616ed7daace25accca
Zer95 <ggrty10@gmail.com>
2022-02-12 20:24:37 +0900

    feat: [Setting] Create

---
+++ Barcodrop/Barcodrop/Setting/Controller/SettingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingViewController.swift
+//  Barcodrop
+//
+//  Created by SG on 2022/01/22.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.white
+    }
+
+}
